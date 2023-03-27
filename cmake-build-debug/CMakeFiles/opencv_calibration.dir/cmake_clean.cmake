d8abd808caace27099c56b11bb2236042a37e6ab
JikunAai <jikunaai@sjtu.edu.cn>
2023-03-27 11:51:30 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/opencv_calibration.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencv_calibration.dir/main.cpp.o"
+  "opencv_calibration.pdb"
+  "opencv_calibration"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencv_calibration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
